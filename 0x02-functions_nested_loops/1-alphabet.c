7007c4222386fbd165ff57b5d285d6954376c090
root <yassmineattar13@gmail.com>
2023-06-20 13:52:07 -0700

    betty styling

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - Entry point
  *
- * Description: print_alphabet - prints the alphabet in lowercase.
+ * print_alphabet - function that use the _putchar function
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
-void print_alphabet(void)
-{
-	int i;
 
+void print_alphabet(void)
+{	int i;
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
-	return (0);
 }
-
