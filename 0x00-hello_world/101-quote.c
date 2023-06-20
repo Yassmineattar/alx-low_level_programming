19645fe1660ab88ae9b8a4a02b53bcc1378f1d52
root <yassmineattar13@gmail.com>
2023-06-19 19:22:23 -0700

    fixing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-int main(){
-	char a[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fputs(a,stdout);
-	return 1;
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fputs(a, stdout);
+	return (1);
 }
