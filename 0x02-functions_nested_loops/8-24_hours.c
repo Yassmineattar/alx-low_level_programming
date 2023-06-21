ed6da354bd6240f16f8ac84a44752c83172ced7f
root <yassmineattar13@gmail.com>
2023-06-20 17:48:05 -0700

    code fixing

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,8 +7,8 @@ void jack_bauer(void)
 {
 	int i, j, k, l;
 
-	for (i = '0'; i <= '2'; i++)
-		for (j = '0'; j <= '3'; j++)
+	for (i = '0'; i <= '1'; i++)
+		for (j = '0'; j <= '9'; j++)
 			for (k = '0'; k <= '5'; k++)
 				for (l = '0'; l <= '9'; l++)
 				{
@@ -19,4 +19,17 @@ void jack_bauer(void)
 					_putchar(l);
 					_putchar('\n');
 				}
+	i = '2';
+
+	for (j = '0'; j <= '3'; j++)
+		for (k = '0'; k <= '5'; k++)
+			for (l = '0'; l <= '9'; l++)
+			{
+				_putchar(i);
+				_putchar(j);
+				_putchar(':');
+				_putchar(k);
+				_putchar(l);
+				_putchar('\n');
+			}
 }
