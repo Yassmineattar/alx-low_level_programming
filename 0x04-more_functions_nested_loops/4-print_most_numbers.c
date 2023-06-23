334a9fe75f1f927974a92a7324fd9fb890395fa2
root <yassmineattar13@gmail.com>
2023-06-22 19:29:43 -0700

    ´changing name of the function

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * print_numbers - function that prints the numbers from 0 to 9 but not 4 and 2
+ * print_most_numbers - function that prints the numbers from 0 to 9
+ * but not 4 and 2
 */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (i == 2 || i == 4)
+		if (i == '2' || i == '4')
 			continue;
-		_putchar(i + '0');
+		_putchar(i);
 	}
 	_putchar('\n');
 }
