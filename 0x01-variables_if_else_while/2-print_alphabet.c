6a5b4f1b588b626a57c4f366aebbe610ddc8b460
root <yassmineattar13@gmail.com>
2023-06-23 17:14:25 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - the entry point
+ * Return: Always success 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}
