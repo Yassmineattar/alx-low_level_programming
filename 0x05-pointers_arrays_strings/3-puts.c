980d7e6009ebfab365a68b5fa94e39f09bcbe187
root <yassmineattar13@gmail.com>
2023-06-30 17:04:29 -0700

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - a function that prints a string
+ *@str: checks input of the function
+*/
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i != 0; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
