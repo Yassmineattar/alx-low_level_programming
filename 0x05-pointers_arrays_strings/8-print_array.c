0b887695ed66a252260520c94477f2c877e33b00
root <yassmineattar13@gmail.com>
2023-07-01 17:26:43 -0700

    a

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_array - a function that prints n elements of an array
+ *@a: checks input of the function
+ *@n: input of the function
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar(a[i]);
+	_putchar('\n');
+}
