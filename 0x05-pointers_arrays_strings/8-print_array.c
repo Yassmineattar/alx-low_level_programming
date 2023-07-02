dd0e2612bdd69995ef775c8b938e467a1549d25c
root <yassmineattar13@gmail.com>
2023-07-01 17:33:18 -0700

    a

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,7 +8,7 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
-		_putchar(a[i] + '0');
-	_putchar('\n');
+	for (i = 0; i < n - 1; i++)
+		printf("%d, ", a[i]);
+	printf("%d\n", a[n - 1]);
 }
