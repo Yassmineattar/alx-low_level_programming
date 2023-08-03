86c05457a377310a86f4d4cad78c84cf4f806d01
root <yassmineattar13@gmail.com>
2023-08-02 19:01:15 -0700

    adding two functions to check if n is prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * is_prime_number - returns 1 if the input integer is a prime numbe
+ *@n:input of the function
+ *@x:input of the function
+ * Return: result
+ */
+int check(int n, int x);
+int is_prime_number(int n)
+{
+	return (check(n, 2));
+}
+/**
+ * check - checks if theres dividers
+ *@n:input of the function
+ *@x:input of the function
+ * Return: result
+ */
+int check(int n, int x)
+{
+	if (x >= n && n > 1)
+		return (1);
+	else if (n % x == 0 || n <= 0)
+		return (0);
+	else
+		return (check(n, x + 1));
+}
