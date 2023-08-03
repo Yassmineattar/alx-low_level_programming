cffb434f5afdd8cdb3e72cb873beaddc4b1c5ceb
root <yassmineattar13@gmail.com>
2023-08-02 17:10:20 -0700

    adding a description to the parameter s

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,9 +3,7 @@
 /**
  * _puts_recursion - prints a string, followed by a new line
  *
- * #s: pointer to the string
- *
- * Return: void
+ * @s: pointer to the string
  */
 
 void _puts_recursion(char *s)
