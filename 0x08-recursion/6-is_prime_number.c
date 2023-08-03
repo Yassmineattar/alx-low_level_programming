6a5cebf197c7fc17a532e646b472c1b46053c261
root <yassmineattar13@gmail.com>
2023-08-02 19:08:09 -0700

    fixing the code for the case of the given number 1

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -20,7 +20,7 @@ int check(int n, int x)
 {
 	if (x >= n && n > 1)
 		return (1);
-	else if (n % x == 0 || n <= 0)
+	else if (n % x == 0 || n <= 1)
 		return (0);
 	else
 		return (check(n, x + 1));
