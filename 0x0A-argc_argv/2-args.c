045632a3f9ad0935752ceb1cf357bc83d0b30f56
root <yassmineattar13@gmail.com>
2023-08-04 19:59:54 -0700

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *@argc:number of arguments
+ *@argv: pointer to array
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+
+	while(argc > 0)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+		argc--;
+	}
+	return (0);
+}
