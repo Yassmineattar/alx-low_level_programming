3db109319a9fca979ea9f895fdb8dd610c9dfd92
root <yassmineattar13@gmail.com>
2023-08-04 19:41:47 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+        return (0);
+}
