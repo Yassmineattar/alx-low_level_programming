e6a0a69433cee9cead904379452fc02b280fe767
root <yassmineattar13@gmail.com>
2023-08-04 19:44:46 -0700

    betty style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,13 @@
 
 /**
  * main - Entry point
- *
+ *@argc:number of arguments
+ *@argv: pointer to array
  * Return: Always 0 (Success)
  */
 int main(int argc, char const *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-        return (0);
+	return (0);
 }
