a4c8423783a29bb20e5792a0788cf16d2c24ded3
Yassmineattar <yassmineattar13@gmail.com>
2023-11-13 05:27:55 +0100

    Dynamic_libraries

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/** 
+ * _memset - prints a string, followed by a new line
+ *@s: pointer to the string
+ *@b: input
+ *@n: input
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n){
+	return (0);
+}
